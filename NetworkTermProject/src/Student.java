a7e9c2bf234c2fea32e268930af4d7176df28b06
heejin ryu <jiin20803@gmail.com>
2019-11-14 19:41:28 +0900

    student 메소듣 생성

--- NetworkTermProject/src/Student.java
+++ NetworkTermProject/src/Student.java
@@ -1,4 +1,28 @@
 
 public class Student {
 
+	String name;
+	String id;
+	String passwd;
+	int win;
+	int lose;
+	int draw;
+	String time;
+	
+	public Student(String name, String id, String passwd) {
+		this.name = name;
+		this.id = id;
+		this.passwd = passwd;
+	}
+	
+	public void win() {this.win++;}
+	public void lose() {this.lose++;}
+	public void draw() {this.draw++;}
+	public String getName() {return name;}
+	public String getID() {return id;}
+	public String getPasswd() {return passwd;}
+	
+	
 }
+
+
