70695b4694c1bb3aa3422c1d69bb255187596d47
heejin ryu <jiin20803@gmail.com>
2019-11-17 21:48:38 +0900

    file object로 append할 때 사용되는 코드 추가

---
+++ NetworkTermProject/src/Login/MyObjectOutputStream.java
@@ -0,0 +1,22 @@
+package Login;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+class MyObjectOutputStream extends ObjectOutputStream
+{
+ public MyObjectOutputStream(OutputStream out) throws IOException
+ {
+  // TODO Auto-generated constructor stub
+  super(out);
+ }
+ 
+ @Override
+ protected void writeStreamHeader() throws IOException
+ {
+  // TODO Auto-generated method stub
+  //헤더저장할때 아무것도 않하기
+	 reset();
+ }
+}
